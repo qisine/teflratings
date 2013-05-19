3367a50f84b327887169f6d44983d8141758f6c5
sk <thanks@no.spammy>
2013-05-19 03:11:21 -0700

    authenticate & registration methods

--- auth.js
+++ auth.js
@@ -1,21 +1,59 @@
-var everyauth = require('everyauth');
+var everyauth = require('everyauth')
+    , bcrypt = require('bcrypt')
+    , mongoose = require('./db')
+    , User = require('./models').User;
 
 everyauth
   .password
+  .loginWith('email')
   .getLoginPath('/signin')
   .postLoginPath('/signin')
   .loginView('<div></div>')
-  .authenticate(function(login, password) {
+  .authenticate(function(email, password) {
+    var promise
+      , errors = [];
 
+    if(!email) errors.push("Email cannot be empty");
+    if(!password) errors.push("Password cannot be empty");
+    if(errors.length) return errors;
+
+    promise = this.Promise();
+    User.findOne({email: email}, function(err, user) {
+      if(err)
+        return promise.fail([err.message || err]);
+      else if(!user)
+        return promise.fail(["User with email [" + email + "] doesn't exist"]);
+
+      bcrypt.compare(password, user.hash, function(err, success) {
+        if(err)
+          return promise.fail([err]);
+        else if(!success)
+          return promise.fail(["Passwords do not match"]);  
+
+        return promise.fulfill(user);
+      }); 
+    });
+    return promise;
   })
   .loginSuccessRedirect('/')
   .getRegisterPath('/register')
   .postRegisterPath('/register')
   .validateRegistration(function(newUserAttrs) {
-
   })
   .registerUser(function(newUserAttrs) {
+    var promise = this.Promise(),
+        passwd = newUserAttrs.password;
 
+    delete newUserAttrs.password;
+    newUserAttrs.salt = bcrypt.genSaltSync(10);
+    newUserAttrs.hash = bcrypt.hashSync(passwd, salt);
+
+    User.create(newUserAttrs, function(err, user) {
+      if(err) return promise.fail(err);
+      return promise.fulfill(user);
+    });
+
+    return promise;
   })
   .registerSuccessRedirect('/');
 
