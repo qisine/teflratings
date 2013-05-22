60b16624c3b0ac041eceaa56df8612b5c7910406
sk <thanks@no.spam>
2013-05-22 08:20:54 -0400

    oversight, the prop is called englishName

--- test/factories/schools.js
+++ test/factories/schools.js
@@ -6,7 +6,7 @@ function createSchool(user, reviews) {
   var s = new School();
   if(user) s.user = user.id;
   if(reviews && reviews.length) s.reviews = reviews.map(function(e) { return e.id });
-  s.name = faker.Company.companyName();
+  s.englishName = faker.Company.companyName();
   s.chineseName = "中文名字";
   s.companyNumber = Math.floor(Math.random() * 1000000000);
   s.street = faker.Address.streetAddress();
